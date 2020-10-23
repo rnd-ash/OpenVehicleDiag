9297f4685440ad508b7d14631eacf85bab2f3c5a
Ashcon Mohseninia <ashconm@outlook.com>
2020-10-23 11:02:14 +0100

    Lib loading

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,13 +1,14 @@
 extern crate gtk;
 use gtk::*;
-use std::process;
 mod app;
+mod drv_select;
+mod passthru;
 
 fn main() {
     gtk::init().expect("Error loading GTK!");
 
-    let app = app::App::new();
+    let mut app = drv_select::DrvSelect::new();
     app.window.show_all();
+    println!("{:?}", app.get_selected_device());
     gtk::main();
-
 }
