fa61a11bebedc7a1361c302d6c01d613d296daf3
Ashcon Mohseninia <ashconm@outlook.com>
2020-11-16 20:03:38 +0000

    ECU Varient detection

--- CBFParser/src/diag.rs
+++ CBFParser/src/diag.rs
@@ -20,13 +20,18 @@ pub enum ServiceType {
     VariantCodingRead = 27
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct DiagService{
     name: Option<String>,
     prep: Vec<DiagPreparation>,
     is_executable: bool,
     client_access_level: u16,
-    security_access_level: u16
+    security_access_level: u16,
+    req_bytes_count: i32,
+    req_bytes_offset: i32,
+    input_ref_name: Option<String>,
+    dataclass_servicetype_shifted: i32,
+    p_count: i32
 }
 
 impl DiagService {
@@ -56,7 +61,7 @@ impl DiagService {
         let r_count = CReader::read_bitflag_i32(&mut bitflags, reader, 0);
         let r_offset = CReader::read_bitflag_i32(&mut bitflags, reader, 0);
 
-        let inputrefnamemaybe = CReader::read_bitflag_string(&mut bitflags, reader, base_addr);
+        let input_ref_name = CReader::read_bitflag_string(&mut bitflags, reader, base_addr);
 
         let u_prep_count = CReader::read_bitflag_i32(&mut bitflags, reader, 0);
         let u_prep_offset = CReader::read_bitflag_i32(&mut bitflags, reader, 0);
@@ -104,36 +109,42 @@ impl DiagService {
             }
         };
 
-        let prep: Vec<DiagPreparation> = (0..u_prep_count as usize).map(|prep_index| {
+        let mut res = DiagService{
+            name,
+            prep: Vec::new(),
+            is_executable: executable > 0,
+            security_access_level,
+            client_access_level,
+            req_bytes_count: req_bytes_count as i32,
+            req_bytes_offset,
+            input_ref_name,
+            dataclass_servicetype_shifted,
+            p_count
+        };
+
+
+        res.prep = (0..u_prep_count as usize).map(|prep_index| {
             let pres_table_offset = base_addr + u_prep_offset as i64;
             reader.seek(pres_table_offset as usize + (prep_index*10));
 
             let prep_entry_offset = reader.read_i32().unwrap() as i64;
             let prep_entry_bitpos = reader.read_i32().unwrap();
             let prep_entry_mode = reader.read_i32().unwrap() as u16;
-
-            DiagPreparation::new(reader, lang, pres_table_offset + prep_entry_offset, prep_entry_bitpos, prep_entry_mode, parent_ecu)
+            DiagPreparation::new(reader, lang, pres_table_offset + prep_entry_offset, prep_entry_bitpos, prep_entry_mode, parent_ecu, &res)
 
         }).collect();
-
-        DiagService{
-            name,
-            prep,
-            is_executable: executable > 0,
-            security_access_level,
-            client_access_level
-        }
-    }
+        res
+    }   
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Dump {
     Text(Option<String>),
     Data(Vec<u8>)
 }
 
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum InferredDataType {
     UnassignedType,
     IntegerType,
@@ -146,7 +157,7 @@ pub enum InferredDataType {
     ExtendedBitDumpType,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct DiagPreparation{
     //parent_ecu: &'a ECU<'a>,
     //parent_diag_service: &'a DiagService,
@@ -157,13 +168,16 @@ pub struct DiagPreparation{
     result_bit_size: i32,
     data_type: InferredDataType,
     system_param: i32,
-    info_pool_idx: i32
+    info_pool_idx: i32,
+    size_in_bits: i32,
+    pres_pool_idx: i32,
+    bit_pos: i32,
 }
 
 
 impl DiagPreparation {
     // parent_diag_service: &'a mut DiagService
-    pub fn new(reader: &mut raf::Raf, lang: &CTFLanguage, base_addr: i64, bit_pos: i32, mode_cfg: u16, parent_ecu: &ECU) -> Self {
+    pub fn new(reader: &mut raf::Raf, lang: &CTFLanguage, base_addr: i64, bit_pos: i32, mode_cfg: u16, parent_ecu: &ECU, parent_diag_service: &DiagService) -> Self {
             
         reader.seek(base_addr as usize);
             let mut bitflags = reader.read_u32().expect("unable to read bitflags!") as u64;
@@ -175,7 +189,7 @@ impl DiagPreparation {
             let alternative_bit_width = CReader::read_bitflag_i32(&mut  bitflags, reader, 0);
             let iit_offset = CReader::read_bitflag_i32(&mut  bitflags, reader, 0);
             let info_pool_idx = CReader::read_bitflag_i32(&mut  bitflags, reader, 0);
-            let pre_pool_idx = CReader::read_bitflag_i32(&mut  bitflags, reader, 0);
+            let pres_pool_idx = CReader::read_bitflag_i32(&mut  bitflags, reader, 0);
             let field_1e = CReader::read_bitflag_i32(&mut  bitflags, reader, 0);
             let system_param = CReader::read_bitflag_i16(&mut  bitflags, reader, -1) as i32;
             let dump_mode = CReader::read_bitflag_i16(&mut  bitflags, reader, 0);
@@ -199,15 +213,18 @@ impl DiagPreparation {
                 alternative_bit_width,
                 result_bit_size: 0,
                 data_type: InferredDataType::UnassignedType,
+                size_in_bits: 0,
                 system_param,
-                info_pool_idx
+                info_pool_idx,
+                pres_pool_idx,
+                bit_pos
                 
             };
-            res.get_size_in_bits(reader, parent_ecu);
+            res.get_size_in_bits(reader, parent_ecu, parent_diag_service);
             res
     }
 
-    fn get_size_in_bits(&mut self, reader: &mut raf::Raf, parent_ecu: &ECU) {
+    fn get_size_in_bits(&mut self, reader: &mut raf::Raf, parent_ecu: &ECU, parent_diag_service: &DiagService) {
         let mode_e = (self.mode_cfg as u32) & 0xF000;
         let mode_h = (self.mode_cfg as u32) & 0xFF0;
         let mode_l = (self.mode_cfg as u32) & 0xF;
@@ -222,6 +239,7 @@ impl DiagPreparation {
 
                 0x320 => {
                     result_bit_size = INT_SIZE_MAPPING[mode_l as usize] as i32;
+                    println!("{}", result_bit_size);
                     self.data_type = InferredDataType::IntegerType;
                 },
                 0x330 => {
@@ -266,9 +284,74 @@ impl DiagPreparation {
                     }
                 } else if mode_e == 0x2000 {
                     self.data_type = InferredDataType::NativePresentationType;
-                }
 
+                    let pres_block = &parent_ecu.presentations;
+                    let pool_bytes = ECU::read_ecu_pool(reader, &pres_block);
+
+                    let mut pool_reader = raf::Raf::from_bytes(&pool_bytes, raf::RafByteOrder::LE);
+
+                    pool_reader.seek(pres_block.entry_size as usize * self.pres_pool_idx as usize);
+
+                    let presentation_struct_offset = pool_reader.read_i32().unwrap();
+                    let presentation_struct_size = pool_reader.read_i32().unwrap();
+
+                    reader.seek((presentation_struct_offset + (pres_block.block_offset)) as usize);
+                    let presentation_struct = reader.read_bytes(presentation_struct_size as usize).expect("Error reading presentation structure!");
+                
+                    let presentation_mode = read_cbf_with_offset(0x1C, &StructureName::PRESENTATION_STRUCTURE, &presentation_struct); // Type
+                    let presentation_length = read_cbf_with_offset(0x1A, &StructureName::PRESENTATION_STRUCTURE, &presentation_struct); // Length
+
+                    if presentation_length > 0 {
+                        result_bit_size = presentation_length;
+                    } else {
+                        result_bit_size = read_cbf_with_offset(0x21, &StructureName::PRESENTATION_STRUCTURE, &presentation_struct);
+                    }
+                    if presentation_mode == 0 {
+                        result_bit_size *= 8; // Bytes to bits
+                    }
+                } else {
+                    panic!("Unknown system type for {}", self.name.clone().unwrap());
+                }
+            } else {
+                if mode_h == 0x410 {
+                    let reduced_sys_param = self.system_param - 0x10;
+                    if reduced_sys_param == 0 {
+                        let res_byte_size = ((parent_diag_service.req_bytes_count & 0xFF) - (self.bit_pos / 8)) * 8;
+                        self.data_type = InferredDataType::ExtendedBitDumpType;
+                        // TODO 
+                    } else if reduced_sys_param == 17 {
+                        let parent = parent_ecu.diag_services.clone();
+                        if let Some(find) = parent.into_iter().find(|mut x| { x.name.as_ref().unwrap() == parent_diag_service.input_ref_name.as_ref().unwrap() }) {
+                            let has_request_data = find.req_bytes_count > 0;
+                            let mut internal_data_type = find.dataclass_servicetype_shifted;
+
+                            if (find.dataclass_servicetype_shifted & 0xC) > 0 && has_request_data {
+                                internal_data_type = match find.dataclass_servicetype_shifted & 4 {
+                                    x if x > 0 => 0x10000000,
+                                    _ => 0x20000000
+                                }
+                            }
+
+                            self.data_type = InferredDataType::UnhandledSP17Type;
+                            if internal_data_type & 0x10000 != 0 {
+                                result_bit_size = find.p_count;
+                            } else {
+                                result_bit_size = parent_diag_service.req_bytes_count * 8;
+                            }
+                        }
+                    }
+                } else if mode_h == 0x420 {
+                    if mode_l > 6 {
+                        panic!("Tryping to map a data type that cannot exist");
+                    }
+                    self.data_type = InferredDataType::IntegerType;
+                    result_bit_size = INT_SIZE_MAPPING[mode_l as usize] as i32;
+                } else if mode_h == 0x430 {
+                    result_bit_size = self.alternative_bit_width;
+                    self.data_type = InferredDataType::BitDumpType
+                }
             }
         }
+        self.result_bit_size = result_bit_size
     }
 }
