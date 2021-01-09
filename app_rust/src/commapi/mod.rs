60242b20ca401510d8e1181c61a2fdb334f8f2b1
Ashcon Mohseninia <ashconm@outlook.com>
2021-01-09 19:21:11 +0000

    Begin adding UDS decoding

--- app_rust/src/commapi/mod.rs
+++ app_rust/src/commapi/mod.rs
@@ -1,3 +1,4 @@
 pub mod comm_api;
 pub mod pdu_api;
 pub mod passthru_api;
+pub mod protocols;
--- app_rust/src/windows/uds_scanner.rs
+++ app_rust/src/windows/uds_scanner.rs
@@ -1,4 +1,4 @@
-use crate::commapi::comm_api::{ComServer, Capability, CanFrame, FilterType};
+use crate::commapi::comm_api::{ComServer, Capability, CanFrame, FilterType, ISO15765Data, ISO15765Config};
 use iced::{Element, Column, Text, Align, Container, Length, Subscription, Row, Checkbox, Rule, Color, Space, button, ProgressBar};
 use iced::time;
 use std::sync::Arc;
@@ -10,6 +10,8 @@ use crate::windows::home::HomeMessage;
 use std::fs::FileType;
 use std::collections::HashMap;
 use iced::widget::pane_grid::TitleBar;
+use std::ops::Index;
+use crate::commapi::protocols::uds::{UDSCommand, UDSRequest};
 
 #[derive(Debug, Clone)]
 pub enum UDSHomeMessage {
@@ -21,6 +23,7 @@ pub enum UDSHomeMessage {
     ScanNextCID(Instant),
     Wait(Instant),
     Listen(Instant),
+    InterrogateECU(Instant)
 }
 
 const MAX_CID_STD: u32 = 0x07FF; // 11bit ID
@@ -28,9 +31,10 @@ const MAX_CID_EXT: u32 = 0x1FFFFFFF; // 29bit - Damn this scan will take forever
 
 #[derive(Debug, Clone, Copy)]
 struct IsoTpResp {
-    id: u32,
+    fc_id: u32,
     bs: u8,
-    st: u8
+    st: u8,
+    uds_support: Option<bool>
 }
 
 #[derive(Debug, Clone)]
@@ -44,6 +48,8 @@ pub struct UDSHome {
     curr_cid: u32,
     ignore_ids: HashMap<u32, CanFrame>,
     listen_duration_ms: u128,
+    interrogation_state: u32,
+    curr_ecu_idx: u32,
     auto_found_ids: Vec<(u32, IsoTpResp)>
 }
 
@@ -59,7 +65,9 @@ impl<'a> UDSHome {
             curr_cid: 0,
             ignore_ids: HashMap::new(),
             auto_found_ids: Vec::new(),
-            listen_duration_ms: 0
+            listen_duration_ms: 0,
+            interrogation_state: 0,
+            curr_ecu_idx: 0
         }
     }
 
@@ -70,6 +78,19 @@ impl<'a> UDSHome {
         }
     }
 
+    pub fn interrogateECU(&mut self, req: UDSRequest, ecu_idx: usize) {
+        let comm_data: (u32, IsoTpResp) = self.auto_found_ids[ecu_idx].clone();
+        let comm_settings = ISO15765Config {
+            send_id: comm_data.0,
+            recv_id: comm_data.1.fc_id,
+            block_size: comm_data.1.bs as u32,
+            sep_time: comm_data.1.st as u32
+        };
+        match req.run_cmd_can(&mut self.server, &comm_settings) {
+            Ok(resp) => println!("ECU Response: {:?}", resp),
+            Err(e) => println!("UDS Error: {:?}", e),
+        }
+    }
 
     pub fn update(&mut self, msg: UDSHomeMessage) -> Option<UDSHomeMessage> {
         match msg {
@@ -100,7 +121,7 @@ impl<'a> UDSHome {
             },
             UDSHomeMessage::ScanNextCID(u) => {
                 if self.curr_cid >= MAX_CID_STD { // Done!
-                    self.scan_stage += 1;
+                    self.scan_stage = 3;
                     // Close the can channel , it is no longer needed
                     self.server.close_can_interface();
                     return None
@@ -114,7 +135,7 @@ impl<'a> UDSHome {
                                 // Also its a frame we haven't seen before!
                                 if self.ignore_ids.get(&candidate.id).is_none() {
                                     // Push the CAN ID that was sent previously and the locate response ID
-                                    self.auto_found_ids.push((self.curr_cid-1, IsoTpResp { id: candidate.id, bs: data[1], st: data[2] }));
+                                    self.auto_found_ids.push((self.curr_cid-1, IsoTpResp { fc_id: candidate.id, bs: data[1], st: data[2], uds_support: None }));
                                     // Also, add the new ID to the ignore list so we don't scan on the Flow control ID
                                     self.ignore_ids.insert(candidate.id, candidate);
                                     break;
@@ -135,7 +156,7 @@ impl<'a> UDSHome {
                 if self.listen_duration_ms >= 5000 {
                     self.scan_stage += 1;
                     // send first bogus CAN Packet
-                    self.curr_cid = 0x0700;
+                    self.curr_cid = 0x07D0;
                     self.get_next_cid();
                     self.server.send_can_packets(&[CanFrame::new(self.curr_cid, &[0x10, 0x64, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])], 0);
                     self.get_next_cid();
@@ -149,6 +170,17 @@ impl<'a> UDSHome {
                     }
                 }
             },
+            UDSHomeMessage::InterrogateECU(_) => {
+                if self.curr_ecu_idx as usize == self.auto_found_ids.len()-1 {
+                    self.scan_stage += 1
+                }
+
+                let req1 = UDSRequest::new(UDSCommand::DiagnosticSessionControl, &[0x92]);
+                let req2 = UDSRequest::new(UDSCommand::TesterPresent, &[0x01]);
+
+                self.interrogateECU(req1, 0);
+                self.interrogateECU(req2, 0);
+            }
         }
         None
     }
@@ -159,6 +191,8 @@ impl<'a> UDSHome {
                 0 => time::every(std::time::Duration::from_millis(4000)).map(UDSHomeMessage::Wait),
                 1 => time::every(std::time::Duration::from_millis(100)).map(UDSHomeMessage::Listen),
                 2 => time::every(std::time::Duration::from_millis(50)).map(UDSHomeMessage::ScanNextCID),
+                3 => Subscription::none(), // Scan summary page
+                4 => time::every(std::time::Duration::from_millis(1000)).map(UDSHomeMessage::InterrogateECU),
                 _ => Subscription::none()
             }
         } else {
@@ -240,19 +274,25 @@ impl<'a> UDSHome {
                     .push(ProgressBar::new((0.0 as f32)..=(0x07FF as f32), self.curr_cid as f32))
                     .push(Text::new("CIDs found"));
                 for (x, y) in &self.auto_found_ids {
-                    c = c.push(Text::new(format!("Found request ID 0x{:04X}, Control found with ID 0x{:04X}. ECU asked for a block size of {} and a separation time of {}ms", x, y.id, y.bs, y.st)))
+                    c = c.push(Text::new(format!("Found request ID 0x{:04X}, Control found with ID 0x{:04X}. ECU asked for a block size of {} and a separation time of {}ms", x, y.fc_id, y.bs, y.st)))
                 }
                 c.into()
             }
-            _ => {
-                println!("{}", self.scan_stage);
+            3 => {
                 let mut c = Column::new()
                     .push(Text::new(format!("Scan completed, found {} possible ECUs", self.auto_found_ids.len())));
                 for (x, y) in &self.auto_found_ids {
-                    c = c.push(Text::new(format!("Potential Send ID 0x{:04X}, FC found with ID 0x{:04X}, asked for block size of {} and a separation time of {}ms", x, y.id, y.bs, y.st)))
+                    c = c.push(Text::new(format!("Potential Send ID 0x{:04X}, FC found with ID 0x{:04X}, asked for block size of {} and a separation time of {}ms", x, y.fc_id, y.bs, y.st)))
                 }
+                c = c.push(button::Button::new(&mut self.auto_state, Text::new("Begin UDS Interrogation")).on_press(UDSHomeMessage::NextMode));
                 c.into()
-            }
+            },
+            4 => {
+                let mut c = Column::new()
+                    .push(Text::new(format!("Scanning ECU  {} possible ECUs", self.auto_found_ids.len())));
+                c.into()
+            },
+            _ => unimplemented!()
         }
     }
 
