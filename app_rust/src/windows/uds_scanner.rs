f84738ad8438099dec0ce05ae04395b590f90b84
Ashcon Mohseninia <ashconm@outlook.com>
2021-01-10 22:00:50 +0000

    Add save UI

--- app_rust/src/windows/uds_scanner.rs
+++ app_rust/src/windows/uds_scanner.rs
@@ -7,11 +7,20 @@ use iced::widget::checkbox::Style;
 use crate::windows::window::WindowMessage;
 use iced::widget::button::State;
 use crate::windows::home::HomeMessage;
-use std::fs::FileType;
+use std::fs::{FileType, File};
 use std::collections::HashMap;
 use iced::widget::pane_grid::TitleBar;
 use std::ops::Index;
+use serde::{Serialize, Deserialize};
 use crate::commapi::protocols::uds::{UDSCommand, UDSRequest, UDSResponse, UDSProcessError};
+use std::io::{Write, Read};
+use crate::themes::dark::Button;
+
+#[derive(Debug, Clone)]
+pub struct ManualMode {
+    ecus: Vec<ECUISOTPSettings>,
+    curr_ecu: ECUISOTPSettings
+}
 
 #[derive(Debug, Clone)]
 pub enum UDSHomeMessage {
@@ -23,7 +32,9 @@ pub enum UDSHomeMessage {
     ScanNextCID,
     Wait(Instant),
     Listen(Instant),
-    InterrogateECU
+    InterrogateECU,
+    OpenFile,
+    SaveScanResults
 }
 
 const MAX_CID_STD: u32 = 0x07FF; // 11bit ID
@@ -32,12 +43,22 @@ const MAX_CID_EXT: u32 = 0x1FFFFFFF; // 29bit - Damn this scan will take forever
 const WAIT_MS: u128 = 5000;
 const LISTEN_MS: u128 = 10000;
 
-#[derive(Debug, Clone, Copy)]
-struct IsoTpResp {
-    fc_id: u32,
-    bs: u8,
-    st: u8,
-    uds_support: Option<bool>
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct CarECUs {
+    vehicle_name: String,
+    vehicle_year: u32,
+    vehicle_oem: String,
+    ecus: Vec<ECUISOTPSettings>
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct ECUISOTPSettings {
+    name: String,
+    send_id: u32,
+    flow_control_id: u32,
+    block_size: u32,
+    sep_time_ms: u32,
+    supports_uds: Option<bool>,
 }
 
 #[derive(Debug, Clone)]
@@ -53,8 +74,9 @@ pub struct UDSHome {
     listen_duration_ms: u128,
     interrogation_state: u32,
     curr_ecu_idx: u32,
-    auto_found_ids: Vec<(u32, IsoTpResp)>,
-    auto_scan_result_ids: Vec<(u32, String)>,
+    auto_found_ids: Vec<(u32, ECUISOTPSettings)>,
+    auto_scan_result_ids: Vec<(u32, bool)>,
+    save_text: String,
 }
 
 impl<'a> UDSHome {
@@ -72,7 +94,8 @@ impl<'a> UDSHome {
             auto_scan_result_ids: Vec::new(),
             listen_duration_ms: 0,
             interrogation_state: 0,
-            curr_ecu_idx: 0
+            curr_ecu_idx: 0,
+            save_text: "".into()
         }
     }
 
@@ -84,19 +107,19 @@ impl<'a> UDSHome {
     }
 
     pub fn interrogateECU(&mut self, req: UDSRequest, ecu_idx: usize) -> Result<UDSResponse, UDSProcessError> {
-        let comm_data: (u32, IsoTpResp) = self.auto_found_ids[ecu_idx].clone();
+        println!("Interrogate ecu {}", ecu_idx);
+        let comm_data: (u32, ECUISOTPSettings) = self.auto_found_ids[ecu_idx].clone();
         let comm_settings = ISO15765Config {
             send_id: comm_data.0,
-            recv_id: comm_data.1.fc_id,
-            block_size: comm_data.1.bs as u32,
-            sep_time: comm_data.1.st as u32
+            recv_id: comm_data.1.flow_control_id,
+            block_size: comm_data.1.block_size,
+            sep_time: comm_data.1.sep_time_ms
         };
         let x = req.run_cmd_can(&mut self.server, &comm_settings);
         match &x {
             Ok(e) => println!("OK: {:?}", e),
             Err(e) => println!("ERR: {:?}", e)
         };
-
         return x;
     }
 
@@ -127,9 +150,55 @@ impl<'a> UDSHome {
             UDSHomeMessage::GoHome => {
                 self.in_home = true;
             },
+            UDSHomeMessage::SaveScanResults => {
+                if self.auto_found_ids.len() > 0 {
+                    let time = chrono::Utc::now();
+                    let path = std::env::current_dir().unwrap().join(format!("scan-{}.ovdjson", time.format("%F-%H_%M_%S")));
+                    match File::create(&path) {
+                        Ok(mut f) => {
+                            let mut ecus :Vec<ECUISOTPSettings> = Vec::new();
+                            for (_, res) in &self.auto_found_ids {
+                                if res.supports_uds.is_some() {
+                                    ecus.push(res.clone())
+                                }
+                            }
+                            let mut car = CarECUs{
+                                vehicle_name: "Unknown".into(),
+                                vehicle_year: 2000,
+                                vehicle_oem: "Unknown".into(),
+                                ecus
+                            };
+                            f.write_all(serde_json::to_string_pretty(&car).unwrap().as_bytes());
+                            self.save_text = format!("Scan results saved to {}", &path.as_os_str().to_str().unwrap());
+                        },
+                        Err(e) => {
+                            self.save_text = format!("Error saving file - {}", e)
+                        }
+                    }
+                }
+            }
+            UDSHomeMessage::OpenFile => {
+                match nfd::open_file_dialog(Some("ovdjson"), None).unwrap_or(nfd::Response::Cancel) {
+                    nfd::Response::Okay(f_path) => {
+                        if let Ok(mut file) = File::open(f_path) {
+                            let mut str = "".into();
+                            file.read_to_string(&mut str);
+
+                            let parse : serde_json::Result<CarECUs>  = serde_json::from_str(&str);
+                            if let Ok(car) = parse {
+                                self.auto_mode = false;
+                                self.
+                            }
+                        }
+                    },
+                    _ => {}
+                }
+            }
+
             UDSHomeMessage::ScanNextCID => {
                 if self.curr_cid >= MAX_CID_STD { // Done!
                     self.scan_stage = 3;
+                    self.curr_ecu_idx = 0;
                     // Close the can channel , it is no longer needed
                     self.server.close_can_interface();
                     return None
@@ -146,7 +215,14 @@ impl<'a> UDSHome {
                                     // Also its a frame we haven't seen before!
                                     if self.ignore_ids.get(&candidate.id).is_none() {
                                         // Push the CAN ID that was sent previously and the locate response ID
-                                        self.auto_found_ids.push((self.curr_cid - 1, IsoTpResp { fc_id: candidate.id, bs: data[1], st: data[2], uds_support: None }));
+                                        self.auto_found_ids.push((self.curr_cid - 1, ECUISOTPSettings {
+                                            name: "Unknown".to_string(),
+                                            send_id: self.curr_cid - 1,
+                                            flow_control_id: candidate.id,
+                                            block_size: data[1] as u32,
+                                            sep_time_ms: data[2] as u32,
+                                            supports_uds: None
+                                        }));
                                         // Also, add the new ID to the ignore list so we don't scan on the Flow control ID
                                         self.ignore_ids.insert(candidate.id, candidate);
                                         found = true;
@@ -180,7 +256,7 @@ impl<'a> UDSHome {
                     println!("Listen complete");
                     self.scan_stage += 1;
                     // send first bogus CAN Packet
-                    self.curr_cid = 0x0000;
+                    self.curr_cid = 0x07D0;
                     self.get_next_cid();
                     self.server.send_can_packets(&[CanFrame::new(self.curr_cid, &[0x10, 0x64, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])], 0);
                     self.get_next_cid();
@@ -199,22 +275,21 @@ impl<'a> UDSHome {
             },
             UDSHomeMessage::InterrogateECU => {
                 self.scan_stage = 4;
-                if self.curr_ecu_idx as usize == self.auto_found_ids.len()-1 {
-                    self.scan_stage += 1;
+                println!("{}", self.curr_ecu_idx);
+                if self.curr_ecu_idx as usize >= self.auto_found_ids.len() {
+                    println!("Scan over");
+                    self.scan_stage = 5;
                     return None;
                 }
-                let ecu_id = self.auto_found_ids[self.curr_ecu_idx as usize].0;
-                let req1 = UDSRequest::new(UDSCommand::TesterPresent, &[0x01]);
-                match self.interrogateECU(req1, self.curr_ecu_idx as usize) {
-                    Ok(_) => self.auto_scan_result_ids.push((ecu_id, "ECU Supports UDS!".into())),
-                    Err(e) => {
-                        match e {
-                            UDSProcessError::CommError(ce) => self.auto_scan_result_ids.push((ecu_id, format!("Failed to scan - Protocol error: {}!", ce))),
-                            UDSProcessError::NoResponse => self.auto_scan_result_ids.push((ecu_id, "ECU did not reply! - Probably doesn't support UDS".into())),
-                            UDSProcessError::InvalidDataLen => self.auto_scan_result_ids.push((ecu_id, "ECU replied with an invalid data length - Probably doesn't support UDS".into())),
-                            UDSProcessError::InvalidErrorCode => self.auto_scan_result_ids.push((ecu_id, "ECU replied with an error which was not recognised! - Probably doesn't support UDS".into())),
-                            UDSProcessError::InvalidCommand => self.auto_scan_result_ids.push((ecu_id, "ECU did not reply with a valid CMD ID! - Probably doesn't support UDS".into())),
-                        }
+                let req = UDSRequest::new(UDSCommand::TesterPresent, &[0x01]);
+                let res = self.interrogateECU(req, self.curr_ecu_idx as usize);
+                match res {
+                    Ok(_) => self.auto_found_ids[self.curr_ecu_idx as usize].1.supports_uds = Some(true),
+                    Err(e) => match e {
+                        UDSProcessError::InvalidCommand => self.auto_found_ids[self.curr_ecu_idx as usize].1.supports_uds = Some(false),
+                        UDSProcessError::InvalidErrorCode => self.auto_found_ids[self.curr_ecu_idx as usize].1.supports_uds = Some(false),
+                        UDSProcessError::InvalidDataLen => self.auto_found_ids[self.curr_ecu_idx as usize].1.supports_uds = Some(false),
+                        _ => {}
                     }
                 }
                 self.curr_ecu_idx += 1;
@@ -259,7 +334,7 @@ impl<'a> UDSHome {
                         .align_items(Align::Center)
                         .push(button::Button::new(&mut self.auto_state, Text::new("Automatic")).on_press(UDSHomeMessage::LaunchAutomatic))
                         .push(Space::with_width(Length::Fill))
-                        .push( button::Button::new(&mut self.manual_state, Text::new("Manual")).on_press(UDSHomeMessage::LaunchManual))
+                        .push( button::Button::new(&mut self.manual_state, Text::new("Manual")).on_press(UDSHomeMessage::OpenFile))
                     ).width(Length::FillPortion(1)))
             .push(Space::with_width(Length::FillPortion(1)))
             .into()
@@ -301,7 +376,7 @@ impl<'a> UDSHome {
                     .push(ProgressBar::new((0.0 as f32)..=(0x07FF as f32), self.curr_cid as f32))
                     .push(Text::new("CIDs found"));
                 for (x, y) in &self.auto_found_ids {
-                    c = c.push(Text::new(format!("Found request ID 0x{:04X}, Control found with ID 0x{:04X}. ECU asked for a block size of {} and a separation time of {}ms", x, y.fc_id, y.bs, y.st)))
+                    c = c.push(Text::new(format!("Found request ID 0x{:04X}, Control found with ID 0x{:04X}. ECU asked for a block size of {} and a separation time of {}ms", x, y.flow_control_id, y.block_size, y.sep_time_ms)))
                 }
                 c.into()
             }
@@ -309,23 +384,35 @@ impl<'a> UDSHome {
                 let mut c = Column::new()
                     .push(Text::new(format!("Scan completed, found {} possible ECUs", self.auto_found_ids.len())));
                 for (x, y) in &self.auto_found_ids {
-                    c = c.push(Text::new(format!("Potential Send ID 0x{:04X}, FC found with ID 0x{:04X}, asked for block size of {} and a separation time of {}ms", x, y.fc_id, y.bs, y.st)))
+                    c = c.push(Text::new(format!("Potential Send ID 0x{:04X}, FC found with ID 0x{:04X}, asked for block size of {} and a separation time of {}ms", x, y.flow_control_id, y.block_size, y.sep_time_ms)))
                 }
                 c = c.push(button::Button::new(&mut self.auto_state, Text::new("Begin UDS Interrogation")).on_press(UDSHomeMessage::InterrogateECU));
                 c.into()
             },
-            _ => {
+            4 => {
                 let curr_ecu_id = if self.curr_ecu_idx as usize >= self.auto_found_ids.len() { self.auto_found_ids.len() - 1 } else { self.curr_ecu_idx as usize };
-                let mut c = Column::new()
+                Column::new()
                     .push(Text::new(format!("Scanning ECU  {} possible ECUs...Currently scanning 0x{:04X}", self.auto_found_ids.len(), self.auto_found_ids[curr_ecu_id].0)))
                     .push(ProgressBar::new((0.0 as f32)..=(self.auto_found_ids.len() as f32), self.curr_ecu_idx as f32))
-                    .push(Space::with_height(Length::Units(10)));
-                for (x, y) in &self.auto_scan_result_ids {
-                    c = c.push(Text::new(format!("Result for ECU 0x{:04X} - {}", x, y)))
-                }
-                c.into()
+                    .push(Space::with_height(Length::Units(10)))
+                    .into()
             },
-            //_ => unimplemented!()
+            5 => {
+                let mut c = Column::new()
+                    .push(Text::new(format!("Scan results")))
+                    .push(Space::with_height(Length::Units(10)));
+                for (x, y) in &self.auto_found_ids {
+                    c = c.push(Text::new(format!("ECU 0x{:04X} - {}", x, match y.supports_uds {
+                        None => "Could not communicate - False positive",
+                        Some(t) => if t { "UDS Supported" } else { "UDS Not supported" }
+                    })));
+                }
+                c = c.push(Space::with_height(Length::Units(5)));
+                c = c.push(button::Button::new(&mut self.auto_state, Text::new("Save results to file")).on_press(UDSHomeMessage::SaveScanResults));
+                c.push(Text::new(&self.save_text))
+                    .into()
+            }
+            _ => unimplemented!()
         }
     }
 
