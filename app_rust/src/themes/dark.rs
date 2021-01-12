00078045ecf3401a5f4ab6d606a5acf9f1962037
Ashcon Mohseninia <ashconm@outlook.com>
2021-01-12 16:30:03 +0000

    Add VIN year decoding

--- app_rust/src/themes/dark.rs
+++ app_rust/src/themes/dark.rs
@@ -1,6 +1,31 @@
 use iced::widget::button::Style;
 use iced::{button, Color};
 use iced::futures::prelude::stream::Collect;
-use crate::themes::BUTTON_RADIUS;
+use crate::themes::*;
 
-pub struct Button;
+pub struct MaterialButton;
+
+impl button::StyleSheet for MaterialButton {
+    fn active(&self) -> Style {
+        button::Style {
+            shadow_offset: Default::default(),
+            background: Some(Color::from_rgb(92.0,19.0,73.0).into()),
+            border_radius: BUTTON_RADIUS,
+            border_width: BUTTON_RADIUS,
+            border_color: Default::default(),
+            text_color: Color::from_rgb(255.0,255.0,255.0)
+        }
+    }
+
+    fn hovered(&self) -> Style {
+        unimplemented!()
+    }
+
+    fn pressed(&self) -> Style {
+        unimplemented!()
+    }
+
+    fn disabled(&self) -> Style {
+        unimplemented!()
+    }
+}
--- app_rust/src/themes/mod.rs
+++ app_rust/src/themes/mod.rs
@@ -1,4 +1,13 @@
 pub mod dark;
 pub mod light;
 
-const BUTTON_RADIUS : f32 = 3.0;
+use iced::{button};
+
+const BUTTON_RADIUS : f32 = 2.0;
+const BUTTON_MIN_WIDTH:f32 = 64.0;
+const BUTTON_MIN_HEIGHT: f32 = 36.0;
+
+pub enum Style {
+    Light,
+    Dark
+}
