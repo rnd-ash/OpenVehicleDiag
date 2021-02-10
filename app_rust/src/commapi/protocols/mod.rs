7404379fc92e7e4df1bd5ad844cb8a47bae2dcd8
Ashcon Mohseninia <ashconm@outlook.com>
2021-02-10 10:21:14 +0000

    Move common server code

--- app_rust/src/commapi/protocols/mod.rs
+++ app_rust/src/commapi/protocols/mod.rs
@@ -1,9 +1,9 @@
-use std::fmt::Display;
+use std::{fmt::Display, time::Instant};
 
 use comm_api::{ComServerError, ISO15765Config};
 use kwp2000::KWP2000ECU;
 
-use super::comm_api::{self, ComServer};
+use super::comm_api::{self, ComServer, ISO15765Data};
 
 pub mod uds;
 pub mod obd2;
@@ -40,8 +40,7 @@ impl ProtocolError {
 
 pub type ProtocolResult<T> = std::result::Result<T, ProtocolError>;
 
-pub trait Selectable {
-    fn get_byte(&self) -> u8;
+pub trait Selectable: Into<u8> {
     fn get_desc(&self) -> String;
     fn get_name(&self) -> String;
 }
@@ -63,7 +62,7 @@ pub trait ECUCommand : Selectable {
 pub trait CommandError {
     fn get_text(&self) -> String;
     fn get_help(&self) -> Option<String>;
-    fn from_byte(b: u8) -> Self where Self: Sized;
+    fn from_byte<'a>(b: u8) -> Self where Self: Sized;
 }
 
 impl std::fmt::Debug for Box<dyn CommandError> {
@@ -150,10 +149,46 @@ impl Drop for DiagServer {
 
 pub trait ProtocolServer: Sized {
     type Command: Selectable + ECUCommand;
+    type Error: CommandError + 'static;
     fn start_diag_session(comm_server: Box<dyn ComServer>, cfg: &ISO15765Config) -> ProtocolResult<Self>;
     fn exit_diag_session(&mut self);
     fn run_command(&self, cmd: u8, args: &[u8], max_timeout_ms: u128) -> ProtocolResult<Vec<u8>>;
     fn read_errors(&self) -> ProtocolResult<Vec<DTC>>;
     fn is_in_diag_session(&self) -> bool;
     fn get_last_error(&self) -> Option<String>;
+
+    fn run_command_isotp(server: &dyn ComServer, send_id: u32, cmd: u8, args: &[u8], receive_require: bool) -> std::result::Result<Vec<u8>, ProtocolError> {
+        let mut data = ISO15765Data {
+            id: send_id,
+            data: vec![],
+            pad_frame: false,
+        };
+        data.data.push(cmd);
+        data.data.extend_from_slice(args);
+        if !receive_require {
+            server.send_iso15765_data(&[data], 0).map(|_| vec![]).map_err(ProtocolError::CommError)
+        } else {
+            // Await max 1 second for response
+            let mut tmp_res = server.send_receive_iso15765(data, 1000, 1).map(|r| r[0].data.clone())?;
+            if tmp_res[0] == 0x7F && tmp_res[2] == 0x78 { // ResponsePending
+                println!("KWP2000 - ECU is processing request - Waiting!");
+                let start = Instant::now();
+                while start.elapsed().as_millis() < 500 {
+                    // ECU is sending a response, but its busy right now. just gotta wait for the ECU to give us its response!
+                    if let Some(msg) = server.read_iso15765_packets(0, 1)?.get(0) {
+                        tmp_res = msg.data.clone();
+                    }
+                }
+            }
+            if tmp_res[0] == 0x7F {
+                // Still error :(
+                Err(ProtocolError::ProtocolError(Box::new(Self::Error::from_byte(tmp_res[2]))))
+            } else if tmp_res[0] == (cmd + 0x40) {
+                Ok(tmp_res)
+            } else {
+                eprintln!("KWP2000 - Command response did not match request? Send: {:02X} - Recv: {:02X}", cmd, tmp_res[0]);
+                Err(ProtocolError::Timeout)
+            }
+        }
+    }
 }
