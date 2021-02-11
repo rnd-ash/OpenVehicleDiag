ff08530707dee5c58dbd2949d0ba96db9b3cc41e
Ashcon Mohseninia <ashconm@outlook.com>
2021-02-11 09:43:06 +0000

    Remove useless array sizes

--- app_rust/src/themes/images.rs
+++ app_rust/src/themes/images.rs
@@ -3,15 +3,15 @@ use iced::{Image, image::Handle};
 
 
 #[cfg(unix)]
-pub const TRAY_ICON: &[u8; 11796] = include_bytes!("../../img/launcher.png");
+pub const TRAY_ICON: &[u8] = include_bytes!("../../img/launcher.png");
 #[cfg(windows)]
-pub const TRAY_ICON: &[u8; 11796] = include_bytes!("..\\..\\img\\launcher.png");
+pub const TRAY_ICON: &[u8] = include_bytes!("..\\..\\img\\launcher.png");
 
 
 #[cfg(unix)]
-pub const LAUNCHER_IMG: &[u8; 32725] = include_bytes!("../../img/logo.png");
+pub const LAUNCHER_IMG: &[u8] = include_bytes!("../../img/logo.png");
 #[cfg(windows)]
-pub const LAUNCHER_IMG: &[u8; 32725] = include_bytes!("..\\..\\img\\logo.png");
+pub const LAUNCHER_IMG: &[u8] = include_bytes!("..\\..\\img\\logo.png");
 
 
 pub fn pix_to_iced_image(src: &'static [u8]) -> iced::Image {
